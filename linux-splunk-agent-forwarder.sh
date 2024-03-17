9373ad9e95648f28731cc3e6d1e69112ebdc7f6c
Francesco Di Sales <f.disales@hotmail.com>
2024-03-17 18:55:35 +0100

    Create linux-splunk-agent-forwarder.sh

---
+++ linux-splunk-agent-forwarder.sh
@@ -0,0 +1,1 @@
+wget -O splunkforwarder-9.2.0.1-d8ae995bf219-Linux-x86_64.tgz "https://download.splunk.com/products/universalforwarder/releases/9.2.0.1/linux/splunkforwarder-9.2.0.1-d8ae995bf219-Linux-x86_64.tgz"
