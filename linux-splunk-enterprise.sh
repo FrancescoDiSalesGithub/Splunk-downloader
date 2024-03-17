0856bd0d8c4329b188e49c581dae908307572eb8
Francesco Di Sales <f.disales@hotmail.com>
2024-03-17 16:03:44 +0100

    using tgz instead of deb

--- linux-splunk-enterprise.sh
+++ linux-splunk-enterprise.sh
@@ -1,1 +1,1 @@
-wget -O splunk-9.2.0.1-d8ae995bf219-linux-2.6-amd64.deb https://download.splunk.com/products/splunk/releases/9.2.0.1/linux/splunk-9.2.0.1-d8ae995bf219-linux-2.6-amd64.deb
+wget -O splunk-9.2.0.1-d8ae995bf219-Linux-x86_64.tgz https://download.splunk.com/products/splunk/releases/9.2.0.1/linux/splunk-9.2.0.1-d8ae995bf219-Linux-x86_64.tgz
